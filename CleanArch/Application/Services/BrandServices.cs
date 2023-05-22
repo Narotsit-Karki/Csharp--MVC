b427ef2b3831e4fb9dd3d418bb84f0492a84dcd2
Narotsit-Karki <narotsitkarki1234@gmail.com>
2023-05-22 14:19:14 +0545

    Added Brand Service to add operations related to Brand Entity
    - Included brand list fetch asyncronously using Dbcontext.
    - Included create student service to add student asyncronously

---
+++ CleanArch/Application/Services/BrandServices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Application.Services
+{
+    public class BrandServices: IBrandServices
+    {
+        private readonly IEDbcontext _dbcontext;
+        public BrandServices(IEDbcontext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+     
+    
+        public async Task<List<Brand>> ListBrandsAsync(CancellationToken cancellationtoken)
+        {
+            var brands = await _dbcontext.Brands.ToListAsync(cancellationtoken);
+            return brands;
+
+        }
+     
+
+        public async Task<int> CreateBrandsAsync(Brand brand,CancellationToken cancellationtoken)
+        {
+            if (brand != null)
+            {
+                _dbcontext.Brands.Add(brand);
+                await _dbcontext.SaveChangesAsync(cancellationtoken);
+
+                return brand.Id;
+            }
+            return -1;
+            
+        }
+    }
+}
