bdf4cd96044f865d2ac2acb9969a87be70f5097e
Narotsit-Karki <narotsitkarki1234@gmail.com>
2023-05-22 14:23:27 +0545

    Registered Brand Service to our API

---
+++ CleanArch/Application/RegisterApplication.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class RegisterApplication
+    {
+        public static void AddApplication(IServiceCollection services) //IServiceCollection is Ioc Contaioners
+        {
+            services.AddScoped<IBrandServices, BrandServices>();
+        }
+    }
+}
